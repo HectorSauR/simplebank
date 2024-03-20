94a044fa89764159b2f02357d09597a9eb13d5de
Hector Sauceda <hector.sauceda.01@gmail.com>
2024-03-19 19:14:17 -0700

    env intialization with viper

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 	"github.com/HectorSauR/simplebank/api"
 	db "github.com/HectorSauR/simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:123@localhost:5433/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
+	"github.com/HectorSauR/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to database", err)
@@ -26,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
